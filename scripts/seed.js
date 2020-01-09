08e472d6a4a2c69fac4f5cc332fb108c681ffd97
Graeme Stewart <gi_stewart@yahoo.com>
2020-01-09 10:07:40 -0500

    push to heroku

--- scripts/seed.js
+++ scripts/seed.js
@@ -9,21 +9,21 @@ var db = require("../models");
 
 db.sequelize
   .sync({ force: true })
-  .then(function() {
-    return seedPlayerProfiles();
-  })
-  .then(function() {
-    return seedPlayerInjuries();
-  })
+  // .then(function() {
+  //   return seedPlayerProfiles();
+  // })
+  // .then(function() {
+  //   return seedPlayerInjuries();
+  // })
   .then(function() {
     return seedPlayerRankings();
-  })
-  .then(function() {
-    return seedPlayerProduction();
-  })
-  .then(function() {
-    return seedPlayerForm();
   });
+// .then(function() {
+//   return seedPlayerProduction();
+// })
+// .then(function() {
+//   return seedPlayerForm();
+// });
 // .then(function() {
 //   db.sequelize.close();
 // });
